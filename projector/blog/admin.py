940abbcc10adb0fda788d9d12d86541aba1b5483
egor_gunkov <egk60793@gmail.com>
2021-04-14 19:19:14 +0300

    field display

--- projector/blog/admin.py
+++ projector/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Articles
 
-admin.site.register(Articles)
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'published')
+
+admin.site.register(Articles, ArticlesAdmin)
