65f890f85ed6274fbab981fc443d95ea6004a425
egk60793 <egk60793@gmail.cpm>
2021-04-26 19:48:09 +0300

    put form in html

--- projector/blog/forms.py
+++ projector/blog/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import *
 
+
 class ArticlesForm(forms.Form):
-    title = forms.CharField(max_legth=150)
+    title = forms.CharField(max_length=150)
     anons = forms.CharField()
     content = forms.CharField()
-    #photo
-    published = forms.BoleanField()
-    rubric = forms.ModelChoiseField(queryset=Category.objects.all)
+    # photo
+    published = forms.BooleanField()
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all())
