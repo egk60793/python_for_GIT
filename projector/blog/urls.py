babf7facdf4689347d85ea887f9c00c78d96f34f
egk60793 <egk60793@gmail.cpm>
2021-04-26 19:28:10 +0300

    start create form 'add article'

--- projector/blog/urls.py
+++ projector/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('public/', public)
+    path('', index, name='home'),
+    path('public/', public, name='public')
 ]
