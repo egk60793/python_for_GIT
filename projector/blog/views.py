5603322697885483ad9ec0c1365b8a48053eb5f5
egk60793 <egk60793@gmail.cpm>
2021-04-26 14:02:41 +0300

    add 'public' page

--- projector/blog/views.py
+++ projector/blog/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 from .models import Articles
 
 
+# главная
 # в переменной blog все поля модели Articles
 def index(request):
     blog = Articles.objects.all()
     return render(request, 'blog/index.html', {'blog': blog})
+
+
+# Опубликоввать статью
+def public(request):
+    return render(request, 'blog/public.html')
