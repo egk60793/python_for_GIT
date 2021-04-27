a7c1d7940139f8a178bce5ea60bbe7e095b06ee5
egk60793 <egk60793@gmail.cpm>
2021-04-27 13:15:15 +0300

    rename field

--- projector/blog/forms.py
+++ projector/blog/forms.py
@@ -3,9 +3,9 @@ from .models import *
 
 
 class ArticlesForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    anons = forms.CharField()
-    content = forms.CharField()
+    title = forms.CharField(max_length=150, label='Название статьи')
+    anons = forms.CharField(max_length=500, label='Анонс статьи')
+    content = forms.CharField(label='Основной контент', widget=forms.Textarea)
     # photo
-    published = forms.BooleanField()
-    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all())
+    published = forms.BooleanField(label='Опубликовать сразу')
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(), label='Выбрать рубрику')
