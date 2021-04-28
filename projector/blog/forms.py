acbecb0bee60df01dd331bf11e93c01eb0a5c5b5
egk60793 <egk60793@gmail.cpm>
2021-04-28 15:18:16 +0300

    model-based form

--- projector/blog/forms.py
+++ projector/blog/forms.py
@@ -2,12 +2,24 @@ from django import forms
 from .models import *
 
 
-class ArticlesForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'myClass'}))
-    anons = forms.CharField(max_length=150, label='Анонос', required=False,
-                            widget=forms.TextInput(attrs={'class': 'myClass'}))
-    content = forms.CharField(label='Контент', widget=forms.Textarea(attrs={'class': 'myClass', 'rows': 5}))
-    # photo
-    published = forms.BooleanField(label='Опубликовать')
-    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(), label='Выбрать рубрику',
-                                    widget=forms.Select(attrs={'class': 'myClass'}))
+# class ArticlesForm(forms.Form):
+#    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'myClass'}))
+#    anons = forms.CharField(max_length=150, label='Анонос', required=False,
+#                            widget=forms.TextInput(attrs={'class': 'myClass'}))
+#    content = forms.CharField(label='Контент', widget=forms.Textarea(attrs={'class': 'myClass', 'rows': 5}))
+#    # photo
+#    published = forms.BooleanField(label='Опубликовать')
+#    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(), label='Выбрать рубрику',
+#                                    widget=forms.Select(attrs={'class': 'myClass'}))
+
+
+class ArticlesForm(forms.ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'content', 'published', 'rubric']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'myClass'}),
+            'anons': forms.TextInput(attrs={'class': 'myClass'}),
+            'content': forms.Textarea(attrs={'class': 'myClass', 'rows': 5}),
+            'rubric': forms.Select(attrs={'class': 'myClass'})
+        }
